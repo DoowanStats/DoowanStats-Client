ab5b53d45e09cd5912436ddb23e0b83ec67e5d61
Jinho Kum <Jinho.kum@gmail.com>
2022-06-28 18:12:55 -0700

    added NextJS and basic paths.

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+// domainName.com/
+
+
+function LandingPage() {
+  return (
+    <div>
+      <div><div>HomeButton</div></div>
+      <div><div>SearchBar</div></div>
+      <div>
+        Current/Past Season Component
+      </div>
+      <div>Not Sponsored By Rito</div>
+    </div>
+  );
+}
+
+export default LandingPage;
